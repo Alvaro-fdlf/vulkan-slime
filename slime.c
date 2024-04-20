5fcb9f6cec5d28a578db2f58ea5febac5a15c6f7
Alvaro Fernandez <alvarofdlf@gmail.com>
2024-04-20 14:20:41 +0200

    Create particles on screen
    Randomly generated positions using srand() and rand(). No direction or
    movement yet.

--- slime.c
+++ slime.c
@@ -2,6 +2,7 @@
  * CONSTANTS TO MODIFY BEHAVIOR AT COMPILE TIME GO HERE
  */
 const int monitorIndex = 0; // Maybe make it command line option later
+#define particleCount 10
 
 #include <stddef.h>
 #include <assert.h>
@@ -28,11 +29,21 @@ const int monitorIndex = 0; // Maybe make it command line option later
 	(x2 > y2) ? x2 : y2; \
 })
 
+typedef struct {
+	double posX, posY, dirX, dirY;
+} particle;
+particle particles[particleCount];
+
 unsigned long long getMicros();
 void draw(uint32_t *buf);
+void genParticle(particle *p);
 
 int main(int argc, char *argv[]) {
 	getDumbBuffers(monitorIndex);
+	srand(getMicros());
+	for (int i=0; i<particleCount; i++) {
+		genParticle(particles + i);
+	}
 
 	while (1) {
 		unsigned long long start = getMicros();
@@ -52,27 +63,13 @@ unsigned long long getMicros() {
 	return tms.tv_sec * 1000000 + tms.tv_nsec/1000;
 }
 
-void draw(uint32_t *buf) {
-	// test by drawing a moving circle
-	for (int i=0; i<xSize*ySize; i++)
-		buf[i] = 0;
-	static int frame = 0;
-	frame++;
-	const int yCenter = ySize/2;
-	const int xCenter = xSize/2;
-	const int radius = min(xSize/3, ySize/3) + sin(frame / 10.0) * 100;
-	const int thickness = 5;
-	const int inner = radius-thickness;
-	const int outer = radius+thickness;
+void genParticle(particle *p) {
+	p->posX = rand() % (xSize/2) + xSize/4;
+	p->posY = rand() % (ySize/2) + ySize/4;
+}
 
-	for (int x=0; x<xSize; x++) {
-		int xDist = abs(x - xCenter);
-		for (int y=0; y<ySize; y++) {
-			int yDist = abs(y - yCenter);
-			int distSquare = yDist*yDist + xDist*xDist;
-			if (distSquare <= outer*outer && distSquare >= inner * inner) {
-				buf[pixel(x, y)] = 0x00FF0000;
-			}
-		}
+void draw(uint32_t *buf) {
+	for (int i=0; i<particleCount; i++) {
+		buf[pixel((int)particles[i].posX, (int)particles[i].posY)] = 0x00FFFFFF;
 	}
 }
