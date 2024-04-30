7e501fafa6e1f573b358a081e97ac9713e1d144f
Alvaro Fernandez <alvarofdlf@gmail.com>
2024-04-30 14:44:09 +0200

    Refactor device info printing code to new function

--- vulkanSetup.c
+++ vulkanSetup.c
@@ -9,43 +9,7 @@ static VkResult result;
 		abort(); \
 	}
 
-void vkSetup () {
-	VkInstance inst;
-	VkApplicationInfo appInfo;
-	VkInstanceCreateInfo instInfo;
-
-	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pNext = NULL;
-	appInfo.pApplicationName = "Slime simulation";
-	appInfo.applicationVersion = 1;
-	appInfo.pEngineName = NULL;
-	appInfo.engineVersion = 0;
-	appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
-
-	instInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	instInfo.pNext = NULL;
-	instInfo.flags = 0;
-	instInfo.pApplicationInfo = &appInfo;
-	instInfo.enabledLayerCount = 0;
-	instInfo.ppEnabledLayerNames = NULL;
-	instInfo.enabledExtensionCount = 0;
-	instInfo.ppEnabledExtensionNames = NULL;
-
-	VkResult result = vkCreateInstance(&instInfo, NULL, &inst);
-	vkFail("Failed to create vulkan instance\n");
-
-	// Enumerate physical devices
-	uint32_t devCount;
-	result = vkEnumeratePhysicalDevices(inst, &devCount, NULL);
-	vkFail("Failed to get amount of physical devices\n");
-
-	printf("This computer has %d Vulkan compatible devices\n", devCount);
-	if (devCount == 0)
-		abort();
-
-	VkPhysicalDevice devs[devCount];
-	result = vkEnumeratePhysicalDevices(inst, &devCount, devs);
-	vkFail("Failed to enumerate physical devices\n");
+static void printPhysicalDevicesInfo(uint32_t devCount, VkPhysicalDevice *devs) {
 	printf("Enumerationg devices available\n");
 	for (int i=0; i<devCount; i++) {
 		VkPhysicalDeviceProperties props;
@@ -101,6 +65,46 @@ void vkSetup () {
 		}
 	}
 	printf("--------------\n");
+}
+
+void vkSetup() {
+	VkInstance inst;
+	VkApplicationInfo appInfo;
+	VkInstanceCreateInfo instInfo;
+
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pNext = NULL;
+	appInfo.pApplicationName = "Slime simulation";
+	appInfo.applicationVersion = 1;
+	appInfo.pEngineName = NULL;
+	appInfo.engineVersion = 0;
+	appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+
+	instInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instInfo.pNext = NULL;
+	instInfo.flags = 0;
+	instInfo.pApplicationInfo = &appInfo;
+	instInfo.enabledLayerCount = 0;
+	instInfo.ppEnabledLayerNames = NULL;
+	instInfo.enabledExtensionCount = 0;
+	instInfo.ppEnabledExtensionNames = NULL;
+
+	VkResult result = vkCreateInstance(&instInfo, NULL, &inst);
+	vkFail("Failed to create vulkan instance\n");
+
+	// Enumerate physical devices
+	uint32_t devCount;
+	result = vkEnumeratePhysicalDevices(inst, &devCount, NULL);
+	vkFail("Failed to get amount of physical devices\n");
+
+	printf("This computer has %d Vulkan compatible devices\n", devCount);
+	if (devCount == 0)
+		abort();
+
+	VkPhysicalDevice devs[devCount];
+	result = vkEnumeratePhysicalDevices(inst, &devCount, devs);
+	vkFail("Failed to enumerate physical devices\n");
+	printPhysicalDevicesInfo(devCount, devs);
 
 	// Create logical device
 	int devInd = 0;
@@ -133,7 +137,7 @@ void vkSetup () {
 	devInfo.ppEnabledLayerNames = NULL;
 	devInfo.enabledExtensionCount = 0;
 	devInfo.ppEnabledExtensionNames = NULL;
-	devInfo.pEnabledFeatures = requiredFeatures;
+	devInfo.pEnabledFeatures = &requiredFeatures;
 
 	VkDevice dev;
 	result = vkCreateDevice(devs[devInd], &devInfo, NULL, &dev);
